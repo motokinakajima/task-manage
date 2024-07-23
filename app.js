7df95ee2383f078a26405141edc4f94097f462ed
motokisama <motokinakajima0912@gmail.com>
2024-07-24 02:18:23 +0900

    added views path

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
 //app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('views', path.join(__dirname, 'views'));
 // Define routes
 app.use('/', indexRouter);
 app.use('/project', projectRouter);
