5e7158dae19337bfbb9a641aaa2b0225547ae147
motokisama <motokinakajima0912@gmail.com>
2024-07-27 00:41:04 +0900

    activated serving static files

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 
 // Define routes
